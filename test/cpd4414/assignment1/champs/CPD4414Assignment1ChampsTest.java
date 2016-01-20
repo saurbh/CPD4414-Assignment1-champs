6a42386cef4d4e605c4a8806b046d93f832da635
c0663965 <c0663965@SB-N208-35.lambton.on.ca>
2016-01-20 16:37:59 -0500

    Created testLessThanOne and testStringAndOne methods.

---
+++ test/cpd4414/assignment1/champs/CPD4414Assignment1ChampsTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment1.champs;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c0663965
+ */
+public class CPD4414Assignment1ChampsTest {
+    
+    public CPD4414Assignment1ChampsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+ 
+    @Test
+    public void testLessThanOneShouldReturnNull() {
+        System.out.println("testLessThanOneShouldReturnNull");
+        String str = "";
+        int a = 0;
+        String expResult = "";
+        String result = CPD4414Assignment1Champs.stringPower(str, a);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testStringAndOneShouldReturnString() {
+        System.out.println("testStringAndOneShouldReturnString");
+        String str = "hello";
+        int a = 1;
+        String expResult = "hello";
+        String result = CPD4414Assignment1Champs.stringPower(str, a);
+        assertEquals(expResult, result);
+    }
+    
+}
